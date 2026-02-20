a2e9e044c51bbee1c56e2665f910868935508493
许愿 <2444654915@qq.com>
2026-02-21 00:11:41 +0800

    使用vue-router最新内置自动路由

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import VueRouter from 'vue-router/vite' // 导入新插件
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    VueRouter({
+      dts: 'src/route-map.d.ts' // 可选：生成类型文件，增强IDE支持
+    }),
     vue(),
     vueJsx(),
     vueDevTools(),
