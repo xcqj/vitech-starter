ddae3f6bc5d1f4fab9323267fed21afc6a9fd66a
许愿 <2444654915@qq.com>
2026-02-21 22:08:30 +0800

    集成unocss icons

--- uno.config.ts
+++ uno.config.ts
@@ -1,8 +1,15 @@
 import presetWind3 from '@unocss/preset-wind3'
-import { defineConfig } from 'unocss'
+import { defineConfig, presetIcons } from 'unocss'
 
 export default defineConfig({
-    presets: [
-        presetWind3(),
-    ],
+  presets: [
+    presetWind3(),
+    presetIcons({
+      prefix: 'i-',
+      extraProperties: {
+        display: 'inline-block',
+      },
+
+    })
+  ],
 })
